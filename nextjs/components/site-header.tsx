74e75eb412cd17c134f28b8c2348dde24849c2ec
edward <bepyan@naver.com>
2023-09-20 15:39:25 +0900

    refactor: nav header

--- nextjs/components/site-header.tsx
+++ nextjs/components/site-header.tsx
@@ -1,10 +1,40 @@
-import { MainNav } from '@/components/main-nav';
+'use client';
+
+import * as React from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
 
 export function SiteHeader() {
+  const pathname = usePathname();
+
+  const menuList = [
+    { path: '/', name: '채팅' },
+    { path: '/translate', name: '한/영 번역기' },
+    { path: '/kakao', name: 'Kakao Developers Helper' },
+  ];
+
   return (
-    <header className='bg-background fixed top-0 z-40 w-full border-b'>
-      <div className='container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0'>
-        <MainNav />
+    <header className='fixed top-0 z-40 w-full h-16 border-b'>
+      <div className='flex h-full gap-6 md:gap-10 px-4 md:px-10'>
+        <Link href='/' className='flex items-center text-zinc-900 hover:text-zinc-700'>
+          <span className='inline-block font-bold'>챗봇</span>
+        </Link>
+        <nav className='flex gap-6'>
+          {menuList.map((menu, i) => (
+            <Link
+              key={i}
+              href={menu.path}
+              className={cn(
+                'flex items-center text-sm font-medium text-zinc-500 hover:text-zinc-700',
+                pathname === menu.path && 'font-semibold text-zinc-700 hover:text-zinc-900',
+              )}
+            >
+              {menu.name}
+            </Link>
+          ))}
+        </nav>
       </div>
     </header>
   );
